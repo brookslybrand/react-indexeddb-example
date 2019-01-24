fef9698cb5f8aaddca6cff1ecd50e66fee820a69
Brooks Lybrand <brooks.lybrand@business-laboratory.com>
2019-01-24 17:22:49 -0600

    Remove console.log statements

--- vanilla-example/src/Form.js
+++ vanilla-example/src/Form.js
@@ -9,73 +9,53 @@ const Form = () => {
   const [lastname, setLastname] = useState('')
 
   useEffect(() => {
-    let db = window.indexedDB.open('FormDatabase', 1)
+    const request = window.indexedDB.open('FormDatabase', 1)
 
-    db.onsuccess = function(event) {
+    request.onsuccess = function(event) {
       // get database from event
-      let db = event.target.result
+      const db = event.target.result
 
       // create transaction from database
-      let transaction = db.transaction('formData', 'readwrite')
-
-      // add success event handleer for transaction
-      // you should also add onerror, onabort event handlers
-      transaction.onsuccess = function(event) {
-        console.log('[Transaction] ALL DONE!')
-      }
+      const transaction = db.transaction('formData', 'readwrite')
 
       // get store from transaction
-      let dataStore = transaction.objectStore('formData')
-
-      /*************************************/
+      const dataStore = transaction.objectStore('formData')
 
       dataStore.get('firstname', 'lastname').onsuccess = function(event) {
         const { result } = event.target
-        console.log('[Transaction - GET] product with id firstname', result)
         if (result && result.value) setFirstname(result.value)
       }
 
       dataStore.get('lastname').onsuccess = function(event) {
         const { result } = event.target
-        console.log('[Transaction - GET] product with id lastname', result)
         if (result && result.value) setLastname(result.value)
       }
     }
 
-    db.onerror = function(event) {
-      console.log('[onerror]', db.error)
+    request.onerror = function(event) {
+      console.log('[onerror]', request.error)
     }
 
-    db.onupgradeneeded = function(event) {
-      let db = event.target.result
+    request.onupgradeneeded = function(event) {
+      const db = event.target.result
       db.createObjectStore('formData', { keyPath: 'id' })
     }
   }, [])
 
   const setName = id => value => {
-    let db = window.indexedDB.open('FormDatabase', 1)
+    const request = window.indexedDB.open('FormDatabase', 1)
 
-    db.onsuccess = function(event) {
+    request.onsuccess = function(event) {
       // get database from event
-      let db = event.target.result
+      const db = event.target.result
 
       // create transaction from database
-      let transaction = db.transaction('formData', 'readwrite')
-
-      // add success event handleer for transaction
-      // you should also add onerror, onabort event handlers
-      transaction.onsuccess = function(event) {
-        console.log('[Transaction] ALL DONE!')
-      }
+      const transaction = db.transaction('formData', 'readwrite')
 
       // get store from transaction
-      let dataStore = transaction.objectStore('formData')
+      const dataStore = transaction.objectStore('formData')
 
       dataStore.put({ id, value }).onsuccess = function(event) {
-        console.log(
-          '[Transaction - PUT] product with id 1',
-          event.target.result
-        )
         window.indexedDB.open('FormDatabase', 1)
       }
 
@@ -83,12 +63,12 @@ const Form = () => {
       if (id === 'lastname') setLastname(value)
     }
 
-    db.onerror = function(event) {
-      console.log('[onerror]', db.error)
+    request.onerror = function(event) {
+      console.log('[onerror]', request.error)
     }
 
-    db.onupgradeneeded = function(event) {
-      let db = event.target.result
+    request.onupgradeneeded = function(event) {
+      const db = event.target.result
       db.createObjectStore('formData', { keyPath: 'id' })
     }
   }
@@ -113,7 +93,6 @@ const Form = () => {
         name="firstname"
         value={firstname}
         onChange={handleSetName('firstname')}
-        // onBlur={e => storeName('firstname')(e.target.value)}
       />
       <br />
       <span>Last name:</span>
@@ -124,7 +103,6 @@ const Form = () => {
         name="lastname"
         value={lastname}
         onChange={handleSetName('lastname')}
-        // onBlur={e => storeName('lastname')(e.target.value)}
       />
       <br />
       <Online>
